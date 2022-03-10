c62ad0706aa26bfb6c404598b1d3f86b7c07c6db
Bilal van Loon <vanloon_1983@hotmail.com>
2022-03-10 20:50:20 +0100

    Changed the child theme not using a plugin

--- functions.php
+++ functions.php
@@ -1,24 +1,8 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
+function cms_webdesign_child_theme_css_scripts() {
+        wp_enqueue_style( 'cms_webdesign_child_theme_css', get_stylesheet_directory_uri() . '/style.css', array( 'cmswebdesign-font-awesome','cmswebdesign-main-style','cmswebdesign-woocommerce-style','cmswebdesign-tablet-style','cmswebdesign-spinner-style' ), '', 'all' );
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
-            $uri = get_template_directory_uri() . '/rtl.css';
-        return $uri;
+        wp_enqueue_script( 'cms_webdesign_child_theme_script', get_stylesheet_directory_uri() . '/scripts.js', array ( 'cmswebdesign-main-script', 'jquery', 'jquery-effects-slide' ), '', true);
     }
-endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-         
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'cmswebdesign-font-awesome','cmswebdesign-main-style','cmswebdesign-woocommerce-style','cmswebdesign-tablet-style','cmswebdesign-spinner-style' ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
-
-// END ENQUEUE PARENT ACTION
+add_action( 'wp_enqueue_scripts', 'cms_webdesign_child_theme_css_scripts', 10 );
