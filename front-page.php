43e21ab83fe808277f4ed84cc783b5d0d03c8bb6
Bilal van Loon <vanloon_1983@hotmail.com>
2022-03-31 22:02:49 +0200

    Change styling header front page

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@
     <div class="overlay">
       <div class="container">
         <div class="header-hero-inside-container">
-          <div class="header-hero-block block-1 container">
+          <div class="header-hero-block block-1">
             <h1>Uitvaartverzorging Ultimum Vale – Een laatste waardige groet!</h1>
             <p>Wij verzorgen in heel NEDERLAND betaalbare uitvaarten met zorg en aandacht voor de nabestaanden en zijn er voor ieder mens, ongeacht de (geloofs)achtergrond van de persoon, waar hij of zij verzekerd is en of er veel geld te besteden is.</p>
             <div class="btn">Meer informatie</div>
