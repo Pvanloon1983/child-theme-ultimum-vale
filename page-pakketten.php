07599d67881a69e606f8c6a96e55b8070c0dbded
Bilal van Loon <vanloon_1983@hotmail.com>
2022-04-27 20:25:44 +0200

    Added pakketten page and overlijden melden page

--- page-pakketten.php
+++ page-pakketten.php
@@ -1,8 +1,7 @@
 <?php get_header(); 
 /**
- * The template for displaying the front page
+ * Template Name: Uitvaartpakketten pagina
  *
- * @package CMS Webdesign starter
  */
 ?>
 
@@ -13,9 +12,8 @@
       <div class="container">
         <div class="header-hero-inside-container">
           <div class="header-hero-block block-1">
-            <h1>Uitvaartverzorging Ultimum Vale – Een laatste waardige groet!</h1>
-            <p>Wij verzorgen in heel NEDERLAND betaalbare uitvaarten met zorg en aandacht voor de nabestaanden en zijn er voor ieder mens, ongeacht de (geloofs)achtergrond van de persoon, waar hij of zij verzekerd is en of er veel geld te besteden is.</p>
-            <div class="btn">Meer informatie</div>
+            <h1><?php the_title(); ?></h1>
+            <p class="page-p-different-header">Wij hebben ervoor gekozen om vijf duidelijke pakketten te maken met verschillende prijzen. Wij denken dat wij daardoor de nabestaanden rust kunnen bieden, terwijl zij de juiste begeleiding krijgen van ons in de dagen voordat de uitvaart gaat plaatsvinden.</p>
           </div>
         </div>
       </div>
@@ -25,7 +23,7 @@
   <section class="section pakketten">
     <div class="container">
 
-    <h2>5 Uitvaartpakketten</h2>
+    <h2>Onze vijf uitvaartpakketten</h3>
 
     <div class="flex-box">
 
@@ -64,39 +62,10 @@
   </section>
   <!-- End section pakketten -->
 
-  <section class="section uitleg" style="background-image: url('<?php echo get_field('eerste_uitleg_afbeelding', 'option'); ?>');           background-position: center center; background-repeat: no-repeat; background-size: cover;">
-  <div class="overlay">
-          <div class="container">    
-          <h3>Wij nemen de tijd om alle beslissingen met u te bespreken</h3>      
-      <div class="uitleg-box">        
-        <div class="box box-1"> 
-          <p>Wij respecteren volledig uw wensen en zullen deze uitvoeren zoals u dat van ons verwacht.
-          Ultimum Vale betekent in het Latijn een laatste groet. De laatste groet naar een overledene is voor iedereen anders. Sommige mensen hechten geen waarde aan de dood. En brengen een laatste groet aan het sterfbed van de overledene, waarna er een crematie in stilte plaats zal vinden.</p>
-          <p>Een ander wil graag een laatste groet brengen in een familiekamer van het crematorium. Terwijl sommige mensen meer waarde hechten aan een compleet afscheid in de aula van het crematorium/begraafplaats. Met eventueel een aansluitend informeel samenzijn in de ontvangstruimte van het crematorium of begraafplaats. Misschien vindt u het juist prettiger om op een andere locatie uw gasten te mogen ontvangen.</p>
-        </div>
-        <div class="box box-2">
-          <p>Uitvaartverzorging Ultimum Vale werkt landelijk. In elke provincie hebben wij deskundige uitvaartbegeleiders die u professioneel bijstaan en alle zorgen rondom de uitvaart voor u uit handen neemt. Onze professionele werkwijze is gebouwd op vele jaren van ervaring. Zo zorgen wij dat de laatste groet van uw dierbare altijd waardig verloopt.</p>
-          <p>Of de overledene nu wel of niet verzekerd is: ook zonder verzekering kunnen wij de uitvaart verzorgen. Als de overledene verzekerd is, bij voorbeeld bij DELA, Yarden, Ardanta etc,  kunnen jullie altijd door ons de uitvaart laten verzorgen. </p>
-          <p><em>- Uitvaartverzorging Ultimum Vale</em></p>
-        </div>
-      </div>
-     
-      </div>
-    </div>
-  </section>
-
-  <section class="section cta-uitvaart-regelen">
-    <div class="container">
-      <h3>Uw uitvaart zoals u dat wilt?</h3>
-      <p>Neem vrijblijvend contact op</p>
-      <button class="cta-uitvaart-regelen btn">Contact opnemen met Ultimum Vale</button>
-    </div>
-  </section>
-
-  <section class="section wat-bieden-wij" style="background-image: url('<?php echo get_field('wat_bieden_wij_afbeelding', 'option'); ?>'); background-position: center center; background-repeat: no-repeat; background-size: cover;">
+  <section class="section wat-bieden-wij">
     <div class="overlay">
       <div class="container">
-        <h3>Wat bieden wij?</h3>
+        <h3>Wat wij nog meer bieden</h3>
         <div class="flex-box">  
           <div class="box">
             <img src="<?php echo get_field('aanbod_kisten_afbeelding', 'option'); ?>" alt="">
@@ -121,22 +90,11 @@
     </div>
   </section>
 
-  <section class="section waardige-uitvaart">
-    <div class="container">
-        <div class="flex-box">
-          <div class="inner-text-box box box-1">
-            <h3>Een waardige & respectvolle uitvaart hoeft niet veel te kosten</h3>
-            <p>Wij hebben ervoor gekozen om <a href="#">vijf duidelijke pakketten</a> te maken met verschillende prijzen. Wij denken dat wij daardoor de nabestaanden rust kunnen bieden, terwijl zij de juiste begeleiding krijgen van ons in de dagen voordat de uitvaart gaat plaatsvinden.</p>
-            <p>Uitvaartverzorging Ultimum Vale  is er om u te ondersteunen in deze moeilijke tijd en zorgt dat deze laatste groet geheel volgens uw wens verloopt.</p>
-          </div>
-    </div>
-  </section>
-
   <?php $begeleid = get_field('begeleid_door', 'option'); ?>
   <section class="section begeleid-door" style="background-image: url('<?php echo $begeleid['begeleid_door_afbeelding']; ?>'); background-position: center center; background-repeat: no-repeat; background-size: cover;">
     <div class="overlay">
       <div class="container">
-      <h3>Wij verzorgen door heel Nederland eerbiedwaardige uitvaarten, begeleid door professionele uitvaartverzorgers uit uw eigen regio.</h3>
+      <h3>Wilt u een overlijden melden of wilt u meer informatie?</h3>
         <div class="flex-box">
           <div class="box">
             <i class="fa-solid fa-envelope"></i> 
@@ -164,6 +122,6 @@
     </div>
   </section>
 
-</main>
+</div>
 
 <?php get_footer(); ?>
--- single-uitvaartpakketten.php
+++ single-uitvaartpakketten.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-  <div class="main-content">
+  <main class="main-content">
 
     <div class="header-hero" style="background-image: url('<?php echo get_field('header_hero_image', 'option'); ?>');           background-position: center center; background-repeat: no-repeat; background-size: cover;">
       <div class="overlay">
@@ -40,7 +40,7 @@
 
           <div class="box-content">
             <div class="box-info">
-              <h3>Wilt u meer informatie?</h3>
+              <h2>Wilt u meer informatie?</h2>
               <p>Heeft u een uitvaartpakket gevonden die bij uw wensen past, of zijn er vragen of onduidelijkheden? Neem gerust contact met ons op.</p>
               <button class="btn">Contact opnemen</button>
             </div>
@@ -49,6 +49,38 @@
       </div>
     </div>
 
-  </div>
+    <?php $begeleid = get_field('begeleid_door', 'option'); ?>
+  <section class="section begeleid-door" style="background-image: url('<?php echo $begeleid['begeleid_door_afbeelding']; ?>'); background-position: center center; background-repeat: no-repeat; background-size: cover;">
+    <div class="overlay">
+      <div class="container">
+      <h3>Wilt u een overlijden melden of wilt u meer informatie?</h3>
+        <div class="flex-box">
+          <div class="box">
+            <i class="fa-solid fa-envelope"></i> 
+            <p><?php echo $begeleid['begeleid_door_email']; ?></p>
+            <button class="btn">
+              Stuur een E-mail
+            </button>
+          </div>
+          <div class="box">
+          <i class="fa-solid fa-phone"></i> 
+            <p><?php echo $begeleid['begeleid_door_telefoonnummer']; ?></p>
+            <button class="btn">
+              Bel ons direct
+            </button>
+          </div>
+          <div class="box">
+            <i class="fa-solid fa-comment-dots"></i>
+            <p><?php echo $begeleid['begeleid_door_bericht']; ?></p>
+            <button class="btn">
+              Naar het contactformulier
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  </main>
 
 <?php get_footer(); ?>
