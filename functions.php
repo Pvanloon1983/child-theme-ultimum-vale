99f5f91dd2985b6c0971066eed9e7280b0196093
Bilal van Loon <vanloon_1983@hotmail.com>
2022-05-05 00:04:38 +0200

    Changed css sheet version

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function cms_webdesign_child_theme_css_scripts() {
         wp_enqueue_style( 'lightboxcss', get_stylesheet_directory_uri(). '/css/lightbox.min.css' , array(), '1.0.0', 'all' );
         wp_enqueue_script( 'lightboxjs', get_stylesheet_directory_uri() . '/js/lightbox.min.js', array( 'jquery' ), '1.0.0', true );
 
-        wp_enqueue_style( 'cms_webdesign_child_theme_css', get_stylesheet_directory_uri() . '/style.css', array( 'cmswebdesign-font-awesome','cmswebdesign-main-style','cmswebdesign-woocommerce-style','cmswebdesign-tablet-style','cmswebdesign-spinner-style' ), '2.1', 'all' );
+        wp_enqueue_style( 'cms_webdesign_child_theme_css', get_stylesheet_directory_uri() . '/style.css', array( 'cmswebdesign-font-awesome','cmswebdesign-main-style','cmswebdesign-woocommerce-style','cmswebdesign-tablet-style','cmswebdesign-spinner-style' ), '2.2', 'all' );
 
         wp_enqueue_script( 'cms_webdesign_child_theme_script', get_stylesheet_directory_uri() . '/scripts.js', array ( 'cmswebdesign-main-script', 'jquery', 'jquery-effects-slide' ), '', true);
     }
