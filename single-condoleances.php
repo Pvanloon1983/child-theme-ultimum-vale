3877aaf6e1ed24ba4002f137c82a52f93bb9336f
Bilal van Loon <vanloon_1983@hotmail.com>
2022-05-09 00:06:42 +0200

    Added single condoleance pagina

--- single-condoleances.php
+++ single-condoleances.php
@@ -17,14 +17,38 @@
   <div class="two-column-section">
     <div class="container">
       <div class="text-content">
-        <?php 
-        if ( have_posts() ) {
-          while ( have_posts() ) {
-            the_post(); 
-            the_content();
-          } // end while
-        } // end if
-        ?>
+        <div class="single-cond-box-top">
+           <div class="image-overledene">
+            <img src="<?php echo get_field('foto_overledene'); ?>" alt="">
+           </div>
+          <div class="info-overledene">
+            <div class="datum-overledene">
+              <span>
+                <i class="fa-solid fa-star-of-life"></i> <?php echo get_field('datum_overleden'); ?> &#8212; 
+                <i class="fa-solid fa-cross"></i> <?php echo get_field('datum_overleden'); ?>
+              </span>
+            </div>
+            <div class="text-overledene">
+              <?php 
+              if ( have_posts() ) {
+                while ( have_posts() ) {
+                  the_post();
+                  the_content();
+                } // end while
+              } // end if
+              ?>
+            </div>
+          </div>
+        </div>
+
+        <div class="condoleance-action">
+          <span>Laat een bericht achter</span>
+        </div>
+
+        <h3>Uw persoonlijke condoleance</h3>
+
+        <?php Ninja_Forms()->display( 4 ); ?>
+
       </div>
       <div class="box-content">
         <div class="box-info">
